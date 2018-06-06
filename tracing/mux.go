691dfaac67f77340767f7c1c7bf9d9c944a26f0e
ycrxun <ycrxun@163.com>
2018-06-06 23:36:52 +0800

    :yum: add HandleFunc

--- tracing/mux.go
+++ tracing/mux.go
@@ -2,12 +2,13 @@ package tracing
 
 import (
 	"net/http"
+
+	"github.com/gorilla/mux"
 	"github.com/opentracing-contrib/go-stdlib/nethttp"
 	"github.com/opentracing/opentracing-go"
-	"github.com/gorilla/mux"
 )
 
-// TracedServeMux is a wrapper around http.ServeMux that instruments handlers for tracing.
+// TracedServeMux is a wrapper around mux.Router that instruments handlers for tracing.
 type TracedServeMux struct {
 	mux    *mux.Router
 	tracer opentracing.Tracer
@@ -21,7 +22,7 @@ func NewServeMux(tracer opentracing.Tracer) *TracedServeMux {
 	}
 }
 
-// Handle implements http.ServeMux#Handle
+// Handle implements mux.Router#Handle
 func (tm *TracedServeMux) Handle(pattern string, handler http.Handler) {
 	middleware := nethttp.Middleware(
 		tm.tracer,
@@ -33,7 +34,20 @@ func (tm *TracedServeMux) Handle(pattern string, handler http.Handler) {
 	tm.mux.Handle(pattern, middleware)
 }
 
-// ServeHTTP implements http.ServeMux#ServeHTTP
+// HandleFunc implements mux.Router#HandleFunc
+func (tm *TracedServeMux) HandleFunc(pattern string, fn func(http.ResponseWriter,
+	*http.Request)) {
+	middleware := nethttp.Middleware(
+		tm.tracer,
+		http.HandlerFunc(fn),
+		nethttp.OperationNameFunc(func(r *http.Request) string {
+			return "HTTP " + r.Method + " " + r.URL.Path
+		}),
+	)
+	tm.mux.Handle(pattern, middleware)
+}
+
+// ServeHTTP implements mux.Router#ServeHTTP
 func (tm *TracedServeMux) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	tm.mux.ServeHTTP(w, r)
 }
